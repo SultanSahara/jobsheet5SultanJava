b605978eeb06ca2007a727ee0f42d3a82e748e82
ASUS <ASUS@DESKTOP-OJM6A95>
2018-08-02 11:22:08 +0700

    Initial Commit

---
+++ src/jobsheet5/OperatorAritmatika.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet5;
+
+/**
+ *
+ * @author ASUS
+ */
+public class OperatorAritmatika {
+    public static void main(String[] args) {
+        //deklarasi
+        int a =10;
+        int b =2;
+        double c;
+        //penjumlahan
+        c = a + b;
+        System.out.println(a+ " + " + b + " = " + c);
+        //pengurangan
+        c = a - b;
+        System.out.println(a+ " - " + b + " = " + c);
+        //perkalian
+        c = a * b;
+        System.out.println(a+ " * " + b + " = " + c);
+        //pembagian
+        c = a / b;
+        System.out.println(a+ ": " + b + " = " + c);
+        //sisa pembagian
+        c = a % b;
+        System.out.println(a+ " % " + b + " = " + c);
+    }
+}
+   
